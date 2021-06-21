f90fc0e55f79ec8291f5efcc3d87d7a6ae26008c
Naomi <41462684+naomixie@users.noreply.github.com>
2021-06-21 23:09:11 +0800

    fix: f

--- src/main.js
+++ src/main.js
@@ -8,16 +8,19 @@ import 'element-ui/lib/theme-chalk/index.css'
 import VueSpeech from 'vue-speech'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Antd from 'ant-design-vue' // 引入Ant Design Vue组件
+import 'ant-design-vue/dist/antd.css' // 引入Ant Design Vue样式
+Vue.use(Antd) //挂载到vue中
 
-Vue.use(VueAxios,axios);
+Vue.use(VueAxios, axios);
 Vue.use(VueSpeech)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,62 +1,162 @@
 <template>
-  <div class="HomePage">
-    <el-container>
-      <el-aside style="width: 50%; margin-left: 30px">
-        <div class="LeftPart" style="margin-top: 0; width: 100%">
-          <div style="margin-top: 10px">
-            <el-input
-              placeholder="请输入内容"
-              v-model="SearchInput"
-              class="input-with-select"
-            >
-              <el-button slot="append" icon="el-icon-search" @click="search" />
-              <el-button slot="append" icon="el-icon-delete" @click="resetSearch" />
-            </el-input>
-          </div>
-          <div style="margin-top: 30px">
-            <AutoCameraView ref="myComent" />
-          </div>
-        </div>
-      </el-aside>
-
-      <el-aside style="width: 50%">
-        <div>
-          <el-button icon="el-icon-setting" style="margin-top: 10px">设置</el-button>
-        </div>
+  <!-- <div class="HomePage"> -->
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header
+        style="
+          background: #fff;
+          box-shadow: 0 2px 8px #f0f1f2;
+          position: relative;
+          z-index: 10;
+          max-width: 100%;
+        "
+      >
         <div
-          class="RightPart"
           style="
-            float: left;
-            margin-left: 15px;
-            width: 80%;
-            height: 100%;
-            margin-top: 30px;
+            position: relative;
+            height: auto;
+            margin-right: 0;
+            margin-left: 0;
+            zoom: 1;
+            display: block;
+            box-sizing: border-box;
           "
         >
-          <el-card class="box-card" style="height: 75%">
-            <h2>欢迎使用慧眼识物</h2>
-            <div v-for="item in OutputContent" :key="item.box" class="text item">
-              <h4>{{ item.classId }} : {{ item.lastTrackedTime }} {{ item.des }}</h4>
-              <!-- {{ item }} -->
-            </div>
-          </el-card>
-
-          <el-popover placement="top-start" title="模式选择" width="200" trigger="hover">
-            <el-switch
-              v-model="DetectMode"
-              active-text="实时检测"
-              inactive-text="长按输入"
+          <a-row>
+            <a-col :span="4" align="start">欢迎来到慧眼识物</a-col>
+            <a-col :span="2" align="start"></a-col>
+            <a-col
+              :span="12"
+              style="
+                border-left: 1px solid #ebedf0;
+                float: left;
+                height: 22px;
+                line-height: 22px;
+                padding-left: 16px;
+                margin: 22px 0;
+              "
             >
-            </el-switch>
-            <el-button slot="reference">模式切换</el-button>
-          </el-popover>
+              <div style="display: flex">
+                <i
+                  aria-label="icon: search"
+                  class="anticon anticon-search"
+                  style="
+                    display: inline-block;
+                    color: inherit;
+                    font-style: normal;
+                    <!-- line-height: 0; -->
+                    text-align: center;
+                    text-transform: none;
+                    <!-- vertical-align: -0.125em; -->
+                    text-rendering: optimizeLegibility;
+                    -webkit-font-smoothing: antialiased;
+                  "
+                  ><svg
+                    viewBox="64 64 896 896"
+                    data-icon="search"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    focusable="false"
+                    class=""
+                  >
+                    <path
+                      d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"
+                    ></path></svg
+                ></i>
+                <a-input
+                  placeholder="请输入内容"
+                  v-model="SearchInput"
+                  class="input-with-select"
+                  style="border: none; display: inline-block; margin: -8px 20px"
+                >
+                  <!-- <el-button slot="append" icon="el-icon-search"  /> -->
+                  <!-- <el-button slot="append" icon="el-icon-delete" @click="resetSearch" /> -->
+                </a-input>
+                <a-button
+                  shape="circle"
+                  icon="search"
+                  @click="search"
+                  style="margin: -8px 10px"
+                />
+                <a-button
+                  shape="circle"
+                  icon="delete"
+                  @click="resetSearch"
+                  style="margin: -8px 0px"
+                />
+                <a-button
+                  shape="circle"
+                  icon="setting"
+                  style="margin: -8px 10px"
+                ></a-button>
+              </div>
+            </a-col>
+            <a-col :span="4"></a-col>
+          </a-row>
         </div>
-      </el-aside>
-    </el-container>
-    <!--    <AudioView/>-->
-    <!--    <OldAudioView/>-->
-    <Record />
+      </a-layout-header>
+      <a-layout-content>
+        <el-container>
+          <!-- <el-header>欢迎来到慧眼识物</el-header> -->
+          <el-aside style="width: 40%; margin-left: 30px">
+            <div class="LeftPart" style="margin-top: 0; width: 100%">
+              <div style="margin-top: 10px"></div>
+              <div style="margin-top: 30px">
+                <AutoCameraView ref="myComent" />
+              </div>
+            </div>
+          </el-aside>
+
+          <el-aside style="width: 50%">
+            <div
+              class="RightPart"
+              style="
+                float: left;
+                margin-left: 15px;
+                width: 80%;
+                height: 100%;
+                margin-top: 30px;
+              "
+            >
+              <el-card class="box-card" style="height: 75%">
+                <h2>欢迎使用慧眼识物</h2>
+                <div v-for="item in OutputContent" :key="item.box" class="text item">
+                  <h4>{{ item.classId }} : {{ item.lastTrackedTime }} {{ item.des }}</h4>
+                  <!-- {{ item }} -->
+                </div>
+              </el-card>
+
+              <el-popover
+                placement="top-start"
+                title="模式选择"
+                width="200"
+                trigger="hover"
+              >
+                <el-switch
+                  v-model="DetectMode"
+                  active-text="实时检测"
+                  inactive-text="长按输入"
+                >
+                </el-switch>
+                <el-button slot="reference">模式切换</el-button>
+              </el-popover>
+            </div>
+          </el-aside>
+        </el-container>
+        <el-container>
+          <Record />
+        </el-container>
+      </a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
   </div>
+
+  <!--    <AudioView/>-->
+  <!--    <OldAudioView/>-->
+
+  <!-- </div> -->
 </template>
 
 <script>
